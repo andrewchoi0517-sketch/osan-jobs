2371d6b10b3bbba652dd419da887b4f6e4bde99e
andrewchoi0517-sketch <andrewchoi0517@gmail.com>
2026-03-09 10:38:25 +0900

    main.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const jobData = [
+    {
+        title: "[정규직]-[서울]-[에스제이피플] 생산관리·기획MD팀 직원 채용 공고",
+        company: "(주)에스제이피플",
+        dept: "공동계열",
+        job: "섬유·의복/섬유제조",
+        location: "서울",
+        url: "https://ostory.osan.ac.kr/ptfol/career/program/rcrt/totRec/findCampRcrtInfo.do?rcrtSeq=2606"
+    },
+    {
+        title: "[정규직]-[경기]-[FAO 한국협회] 회계·총무직 직원 채용 공고",
+        company: "FAO 한국협회",
+        dept: "인문사회계열",
+        job: "경영·회계·사무/재무·회계",
+        location: "경기",
+        url: "https://ostory.osan.ac.kr/ptfol/career/program/rcrt/totRec/findCampRcrtInfo.do?rcrtSeq=2603"
+    },
+    {
+        title: "[정규직]-[경기]-[유니테스트] 반도체 장비 제어 및 소프트웨어 개발 채용",
+        company: "유니테스트",
+        dept: "공학계열",
+        job: "IT/소프트웨어개발",
+        location: "경기",
+        url: "https://ostory.osan.ac.kr/ptfol/career/program/rcrt/totRec/findCampRcrtInfo.do?rcrtSeq=2600"
+    }
+    // 더 많은 데이터를 여기에 추가할 수 있어요!
+];
